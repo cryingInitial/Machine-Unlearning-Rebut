d26855fe376d84ea5bba86b139622914cc776e7a
cryingInitial <specific0924@gmail.com>
2024-10-27 16:15:16 +0900

    scripts update

--- scripts/cifar.sh
+++ scripts/cifar.sh
@@ -11,6 +11,6 @@ do
         gpu_idx=$((4 * eps_idx + seed_idx))
         CUDA_VISIBLE_DEVICES=${GPUS[$gpu_idx]} python3 audit_model.py --data_name cifar10_half --model_name cnn --n_epochs 200 --lr 8e-5 --epsilon ${EPS[$eps_idx]} \
             --fixed_init --target_type $TARGET_TYPE --n_reps 256 \
-            --seed ${SEEDS[$seed_idx]} --out exp_data/cifar/seed${SEEDS[$seed_idx]}/ --block_size 1250 > exp_data/cifar/seed${SEEDS[$seed_idx]}/${EPS[$eps_idx]}.log 2>&1 &
+            --seed ${SEEDS[$seed_idx]} --out exp_data/cifar_$TARGET_TYPE/seed${SEEDS[$seed_idx]}/ --block_size 1250 > exp_data/cifar/seed${SEEDS[$seed_idx]}/${EPS[$eps_idx]}.log 2>&1 &
     done
 done
--- scripts/mnist.sh
+++ scripts/mnist.sh
@@ -1,5 +1,5 @@
 SEEDS=(0 1 2 3 4 5 6 7)
-EPS=(10.0)
+EPS=(4.0)
 GPUS=(0 1 2 3 4 5 6 7)
 TARGET_TYPE="blank"
 
@@ -12,6 +12,6 @@ do
         gpu_idx=$((4 * eps_idx + seed_idx))
         CUDA_VISIBLE_DEVICES=${GPUS[$gpu_idx]} python3 audit_model.py --data_name mnist_half --model_name cnn --n_epochs 100 --lr 1.33e-4 --epsilon ${EPS[$eps_idx]} \
             --fixed_init --target_type $TARGET_TYPE --n_reps 256 \
-            --seed ${SEEDS[$seed_idx]} --out exp_data/mnist/seed${SEEDS[$seed_idx]}/ --block_size 10000 > exp_data/mnist/seed${SEEDS[$seed_idx]}/${EPS[$eps_idx]}.log 2>&1 &
+            --seed ${SEEDS[$seed_idx]} --out exp_data/mnist_$TARGET_TYPE/seed${SEEDS[$seed_idx]}/ --block_size 10000 > exp_data/mnist/seed${SEEDS[$seed_idx]}/${EPS[$eps_idx]}.log 2>&1 &
     done
 done
